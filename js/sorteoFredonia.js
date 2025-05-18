a689e0e1792769b2f643753e1f4f5d1f6c2c2546
Alejandro <alejooliveros29@gmail.com>
2025-05-17 19:29:59 -0500

    Cambios en ruletas y sorteos js

--- js/sorteoFredonia.js
+++ js/sorteoFredonia.js
@@ -1,68 +1,21 @@
-const miRuleta = new Winwheel({
-  'numSegments': 50,
-  'outerRadius': 250,
-  'segments': [
-      { 'fillStyle': '#70F538', 'text': 'Jugador #49' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #15' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #18' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #33' },
-      { 'fillStyle': '#F502E3', 'text': 'Jugador #4' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #48' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #22' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #2' },
-      { 'fillStyle': '#26E8F5', 'text': 'Jugador #26' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #19' },
-      { 'fillStyle': '#A20A6A', 'text': 'Jugador #36' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #16' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #45' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #7' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #24' },
-      { 'fillStyle': '#F502E3', 'text': 'Jugador #34' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #21' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #46' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #38' },
-      { 'fillStyle': '#26E8F5', 'text': 'Jugador #40' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #35' },
-      { 'fillStyle': '#A20A6A', 'text': 'Jugador #25' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #8' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #23' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #50' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #27' },
-      { 'fillStyle': '#F502E3', 'text': 'Jugador #20' },//aqui
-      { 'fillStyle': '#70F538', 'text': 'Jugador #37' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #10' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #44' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #30' },
-      { 'fillStyle': '#F502E3', 'text': 'Jugador #42' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #47' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #28' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #5' },
-      { 'fillStyle': '#26E8F5', 'text': 'Jugador #13' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #31' },
-      { 'fillStyle': '#A20A6A', 'text': 'Jugador #1' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #17' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #29' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #6' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #41' },//aqui
-      { 'fillStyle': '#70F538', 'text': 'Jugador #14' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #32' },
-      { 'fillStyle': '#d30000', 'text': 'Jugador #3' },
-      { 'fillStyle': '#F502E3', 'text': 'Jugador #43' },
-      { 'fillStyle': '#70F538', 'text': 'Jugador #9' },
-      { 'fillStyle': '#F5F31A', 'text': 'Jugador #12' },
-      { 'fillStyle': '#1B3AF5', 'text': 'Jugador #39' },
-      { 'fillStyle': '#A20A6A', 'text': 'Jugador #11' },
-      
-  ],
-  'animation': {
-      'type': 'spinToStop',
-      'duration': 5,
-      'callbackFinished': 'Mensaje()',
-      'callbackAfter': 'sonar()',
-      'callbackAfter': 'dibujarIndicador()'
-  }
-});
-sonar();
+Ruleta(31);
+
+function Ruleta (players){
+  const randomPlayers = generateRandomPlayers(players);
+  const miRuleta = new Winwheel({
+    'numSegments': players,
+    'outerRadius': 250,
+    'segments': randomPlayers,
+    'animation': {
+        'type': 'spinToStop',
+        'duration': 5,
+        'callbackFinished': 'Mensaje()',
+        'callbackAfter': 'sonar()',
+        'callbackAfter': 'dibujarIndicador()'
+    }
+  });
+  sonar();
+}
 function sonar() {
   let boton = document.querySelector("#ruleta");
 
@@ -100,4 +53,40 @@ function dibujarIndicador() {
 
 function carga_sonido() {
   document.getElementById('sorteo').innerHTML = '<embed src="../audio/Celebracion.mp3" autostart="true" />'
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function getRandomBrightColor() {
+  const getBrightComponent = () => Math.floor(Math.random() * 128) + 128; // Valores entre 128 y 255
+  const r = getBrightComponent().toString(16).padStart(2, '0');
+  const g = getBrightComponent().toString(16).padStart(2, '0');
+  const b = getBrightComponent().toString(16).padStart(2, '0');
+  return `#${r}${g}${b}`;
+}
+
+function generateRandomPlayers(totalPlayers) {
+  const players = [];
+  const usedNumbers = new Set();
+
+  for (let i = 0; i < totalPlayers; i++) {
+      // Generar un número único para el jugador
+      let playerNumber;
+      do {
+          playerNumber = Math.floor(Math.random() * totalPlayers) + 1; // Números entre 1 y 100
+      } while (usedNumbers.has(playerNumber));
+      usedNumbers.add(playerNumber);
+
+      // Crear el objeto del jugador
+      const player = {
+          fillStyle: getRandomBrightColor(),
+          text: `Jugador #${playerNumber}`
+      };
+
+      players.push(player);
+  }
+
+  return players;
 }
--- js/sorteoGardel.js
+++ js/sorteoGardel.js
@@ -1,68 +1,21 @@
-const miRuleta = new Winwheel({
-  'numSegments': 50,
-  'outerRadius': 250,
-  'segments': [
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #49' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #15' },
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #18' },
-      { 'fillStyle': '#033D75', 'text': 'Jugador #33' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #4' }, 
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #48' },/**Aqi */
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #22' },/** */
-      { 'fillStyle': '#102241', 'text': 'Jugador #2' },/** */
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #26' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #19' },
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #36' },
-      { 'fillStyle': '#033D75', 'text': 'Jugador #16' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #45' },
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #7' },
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #24' },
-      { 'fillStyle': '#102241', 'text': 'Jugador #34' },
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #21' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #46' },
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #38' },
-      { 'fillStyle': '#033D75', 'text': 'Jugador #40' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #35' },
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #25' },
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #8' },
-      { 'fillStyle': '#102241', 'text': 'Jugador #23' },
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #50' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #27' },
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #20' },//aqui
-      { 'fillStyle': '#033D75', 'text': 'Jugador #37' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #10' },
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #44' },
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #30' },
-      { 'fillStyle': '#102241', 'text': 'Jugador #42' },
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #47' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #28' },
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #5' },
-      { 'fillStyle': '#033D75', 'text': 'Jugador #13' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #31' },
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #1' },
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #17' },
-      { 'fillStyle': '#102241', 'text': 'Jugador #29' },
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #6' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #41' },//aqui
-      { 'fillStyle': '#FEDD94', 'text': 'Jugador #14' },
-      { 'fillStyle': '#033D75', 'text': 'Jugador #32' },
-      { 'fillStyle': '#FCD555', 'text': 'Jugador #3' },
-      { 'fillStyle': '#B82A45', 'text': 'Jugador #43' },
-      { 'fillStyle': '#F4AA43', 'text': 'Jugador #9' },
-      { 'fillStyle': '#102241', 'text': 'Jugador #12' },
-      { 'fillStyle': '#9C6925', 'text': 'Jugador #39' },
-      { 'fillStyle': '#F12E51', 'text': 'Jugador #11' },
-      
-  ],
-  'animation': {
-      'type': 'spinToStop',
-      'duration': 5,
-      'callbackFinished': 'Mensaje()',
-      'callbackAfter': 'sonar()',
-      'callbackAfter': 'dibujarIndicador()'
-  }
-});
-sonar();
+Ruleta(50);
+
+function Ruleta (players){
+  const randomPlayers = generateRandomPlayers(players);
+  const miRuleta = new Winwheel({
+    'numSegments': players,
+    'outerRadius': 250,
+    'segments': randomPlayers,
+    'animation': {
+        'type': 'spinToStop',
+        'duration': 5,
+        'callbackFinished': 'Mensaje()',
+        'callbackAfter': 'sonar()',
+        'callbackAfter': 'dibujarIndicador()'
+    }
+  });
+  sonar();
+}
 function sonar() {
   let boton = document.querySelector("#ruleta");
 
@@ -100,4 +53,40 @@ function dibujarIndicador() {
 
 function carga_sonido() {
   document.getElementById('sorteo').innerHTML = '<embed src="../audio/Celebracion.mp3" autostart="true" />'
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function getRandomBrightColor() {
+  const getBrightComponent = () => Math.floor(Math.random() * 128) + 128; // Valores entre 128 y 255
+  const r = getBrightComponent().toString(16).padStart(2, '0');
+  const g = getBrightComponent().toString(16).padStart(2, '0');
+  const b = getBrightComponent().toString(16).padStart(2, '0');
+  return `#${r}${g}${b}`;
+}
+
+function generateRandomPlayers(totalPlayers) {
+  const players = [];
+  const usedNumbers = new Set();
+
+  for (let i = 0; i < totalPlayers; i++) {
+      // Generar un número único para el jugador
+      let playerNumber;
+      do {
+          playerNumber = Math.floor(Math.random() * totalPlayers) + 1; // Números entre 1 y 100
+      } while (usedNumbers.has(playerNumber));
+      usedNumbers.add(playerNumber);
+
+      // Crear el objeto del jugador
+      const player = {
+          fillStyle: getRandomBrightColor(),
+          text: `Jugador #${playerNumber}`
+      };
+
+      players.push(player);
+  }
+
+  return players;
 }
